44ea6780f384bd4c39d47fa14aa4a17a7b67c34a
Ishu Srivastava <ishu.cs@gmail.com>
2024-03-12 23:48:58 +0530

    folder structure created

---
+++ templete.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+import logging
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "textSummarizer"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath) # to convert the file path as per the native OS
+    filedir,filename = os.path.split(filepath) # to split the path into folder name and file name and assign them into 2 seperate variables
+
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True) # exit_ok=True will create the folder if it's not there already else it will skip creation
+        logging.info(f"Created folder : {filedir} for the file {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Created empty file : {filename}")   
+    else:
+        logging.info(f"{filename} already exists") 
+
